dc70b73d353fb90d322ff4350a1a3930c3f973c3
rageandqq <chitley.sameer@gmail.com>
2016-10-02 14:30:33 -0400

    Added accept and reject buttons with styling.

--- app/components/CameraComponent.js
+++ app/components/CameraComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
   Dimensions,
+  Image,
   StyleSheet,
   View,
 } from 'react-native';
@@ -9,13 +10,46 @@ import Button from 'react-native-button';
 
 export default class CameraComponent extends Component {
 
+  state = {
+    image: null,
+  };
+
   _takePicture() {
     this.camera.capture()
-      .then((data) => console.log(data)) // TODO(@rageandqq): Process image
+      .then((data) => {
+        console.log(data.path);
+        console.log(typeof(data.path));
+        this.setState({image: data.path});
+      }) // TODO(@rageandqq): Process image
       .catch(err => console.error(err));
   }
 
   render() {
+    const { image } = this.state;
+    if (image != null) {
+      return (
+        <View
+          style={styles.container}>
+          <Image
+            style={styles.camera}
+            source={{uri: image}}/>
+            <View
+              style={styles.buttonGroupContainer}>
+              <Button
+                containerStyle={styles.buttonAcceptContainer}
+                style={styles.button}>
+                Accept
+              </Button>
+              <Button
+                containerStyle={styles.buttonRejectContainer}
+                onPress = {()=>{ this.setState({image: null})}}
+                style={styles.button}>
+                Retake
+              </Button>
+            </View>
+        </View>
+      );
+    }
     return (
       <View style={styles.container}>
         <Camera
@@ -23,9 +57,10 @@ export default class CameraComponent extends Component {
             this.camera = camera;
           }}
           style={styles.camera}
-          aspect={Camera.constants.Aspect.fill}>
+          aspect={Camera.constants.Aspect.fill}
+          captureTarget={Camera.constants.CaptureTarget.temp}>
           <Button
-            containerStyle={styles.buttonContainer}
+            containerStyle={styles.buttonInfoContainer}
             style={styles.button}
             onPress={this._takePicture.bind(this)}>
             Capture
@@ -35,6 +70,13 @@ export default class CameraComponent extends Component {
     );
   }
 }
+
+const buttonStyles = {
+  padding: 10,
+  margin: 20,
+  overflow: 'hidden',
+  borderRadius: 10,
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -52,12 +94,27 @@ const styles = StyleSheet.create({
     fontSize: 20,
     flex: 1,
   },
-  buttonContainer: {
-    padding: 10,
-    margin: 20,
-    height: 45,
-    overflow: 'hidden',
-    borderRadius: 10,
+  buttonInfoContainer: {
     backgroundColor: 'deepskyblue',
+    ...buttonStyles,
   },
+  buttonGroupContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 20,
+    right: 30,
+    marginBottom: 50,
+    marginRight: 50,
+    ...buttonStyles,
+  },
+  buttonAcceptContainer: {
+    flex: 1,
+    backgroundColor: 'limegreen',
+    ...buttonStyles,
+  },
+  buttonRejectContainer: {
+    flex: 1,
+    backgroundColor: 'red',
+    ...buttonStyles,
+  }
 });
